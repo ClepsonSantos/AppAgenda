c1fe1ab25dc4ad26715f6ff2dc4935ae1ea0c7d2
ClepsonSantos <clepsonsantos9@gmail.com>
2018-06-13 21:29:21 -0300

    first commit,,

---
+++ app/src/main/java/com/example/lab1/appagenda/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lab1.appagenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String nome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
