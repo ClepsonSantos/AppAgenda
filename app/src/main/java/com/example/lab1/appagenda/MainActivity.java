880b3f1ee35d11b543e203a4b51a6bc1f66e386d
Clepson Santos <clepsonsantos9@gmail.com>
2018-06-18 14:47:10 -0300

    criado os botões na tela principal,, 👽

--- app/src/main/java/com/example/lab1/appagenda/MainActivity.java
+++ app/src/main/java/com/example/lab1/appagenda/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.lab1.appagenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAdicionar, btnLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdicionar = (Button) findViewById(R.id.btnAdicionar);
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        this.btnLista = (Button) findViewById(R.id.btnLista);
+        this.btnLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
